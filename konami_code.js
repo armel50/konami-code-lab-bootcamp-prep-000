336e55a5d1d255e5ec26f637a8251ac8fd061c87
Cloe Kouadjo <armelfli50@gmail.com>
2019-06-27 23:11:32 +0000

    Automatically backed up by Learn

--- konami_code.js
+++ konami_code.js
@@ -10,7 +10,33 @@ const codes = [
   "b",
   "a"
 ];
+var check=[];
 
 function init() {
-  // your code here
+
+  document.addEventListener("keydown",function(e){
+    const key =e.key;
+    
+    for(var i = 0; i<codes.length;i++){
+      if(key===codes[i]){
+        console.log(key);
+        check.push(key);
+        
+        if(check.length===10){
+         console.log(check);
+         console.log(codes);
+         if(check===codes){
+           console.log("Typ");
+         }
+          
+        }
+        break;
+      }
+      
+      
+    }
+      
+    });
+
+   
 }
