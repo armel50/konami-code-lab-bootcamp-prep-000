5eabeccca3cf1ad190599641b8eea9a3ed6a2d32
Cloe Kouadjo <armelfli50@gmail.com>
2019-06-28 00:30:19 +0000

    Automatically backed up by Learn

--- konami_code.js
+++ konami_code.js
@@ -42,7 +42,7 @@ function init() {
             }else{
                 //console.log("yessssssssss");
                 
-                alert("Yessssssss");
+                console.log("Yessssssss");
                 check.length =0;
                  trueOrFalse.length=0;
             }
